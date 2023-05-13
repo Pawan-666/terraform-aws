8c44bf8a786ba0baffa0135ac40a156dc04394a8
Pawan-666 <eulogy1996@gmail.com>
2023-05-13 19:52:50 +0545

    2048 game on public ip

--- user_data.sh
+++ user_data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-yum update -y
-yum install httpd -y
-systemctl start httpd
-systemctl enable httpd
-echo "<h1>Hello from aws</h1>" > /var/www/html/index.html
-systemctl restart httpd
+sudo yum update -y
+sudo yum install httpd git -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo git clone https://github.com/gabrielecirulli/2048.git
+sudo cp -R 2048/* /var/www/html/
+sudo systemctl restart httpd
